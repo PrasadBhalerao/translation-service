ca192b83ed303e1ac13707103bf3858f352cb946
Prasad Bhalerao <prasad.bhalerao985@gmail.com>
2018-10-15 21:10:02 +0530

    Added default page

--- TranslationService/TranslationService.Web/App_Start/RouteConfig.cs
+++ TranslationService/TranslationService.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TranslationService.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
